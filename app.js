06227691f649fa75641be3d5275591d7217f52c9
mathisang <mathisang@users.noreply.github.com>
2018-07-25 18:03:11 +0200

    Final version

---
+++ app.js
@@ -0,0 +1,45 @@
+document.getElementById("one").onmouseover = function() {
+  document.getElementById("title").innerText = "World Cup 2018 Pronostics";
+  document.getElementById("details").innerText = "[personnal project]";
+};
+
+document.getElementById("one").onmouseout = function() {
+    document.getElementById("title").innerText = "Mathis Angibaud";
+    document.getElementById("details").innerText = "i'm looking for a professional training contract";
+};
+
+
+
+document.getElementById("two").onmouseover = function() {
+    document.getElementById("title").innerText = "Ouest Habitat Conseil";
+    document.getElementById("details").innerText = "[professional project]";
+};
+
+document.getElementById("two").onmouseout = function() {
+    document.getElementById("title").innerText = "Mathis Angibaud";
+    document.getElementById("details").innerText = "i'm looking for a professional training contract";
+};
+
+
+
+document.getElementById("three").onmouseover = function() {
+    document.getElementById("title").innerText = "Choose";
+    document.getElementById("details").innerText = "[university project]";
+};
+
+document.getElementById("three").onmouseout = function() {
+    document.getElementById("title").innerText = "Mathis Angibaud";
+    document.getElementById("details").innerText = "i'm looking for a professional training contract";
+};
+
+
+
+document.getElementById("four").onmouseover = function() {
+    document.getElementById("title").innerText = "Zoo de Beauval";
+    document.getElementById("details").innerText = "[personnal project]";
+};
+
+document.getElementById("four").onmouseout = function() {
+    document.getElementById("title").innerText = "Mathis Angibaud";
+    document.getElementById("details").innerText = "i'm looking for a professional training contract";
+};
